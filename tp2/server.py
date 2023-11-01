12e31690ec1acc62f2e1c9761c2ae02359bd7e14
72Rogan <danieldu109@gmail.com>
2023-11-01 20:06:03 +0000

    Still errors in locks, but now multithreads are fixed

--- tp2/server.py
+++ tp2/server.py
@@ -3,48 +3,35 @@ import threading
 import time
 import sys
 
-
 class Database:
     def __init__(self):
         self.dados = {"coisas": 3, "jmf": -5, "redes": 1}
         self.lock = threading.Lock()
 
-#----------------------------------------- -------AUXILIARY FUNCTIONS---------------------------------------------
-
-#function to parse the arguments given in command line and return the server ip and port
+# Function to parse the arguments given in the command line and return the server IP and port
 def rec_args(N_porta):
-    # In Case of giving the wrong command
     if len(sys.argv) < 2:
         print("Usage: python3 server.py <server_ip:server_port>")
         sys.exit(1)
 
-    # Splitting the argument to extract the server IP and server port
-        if N_porta==1:
-            server_ip, server_port = sys.argv[1].split(":")
-        elif N_porta==2:
-            server_ip, server_port = sys.argv[2].split(":")
+    if N_porta == 1:
+        server_ip, server_port = sys.argv[1].split(":")
+    elif N_porta == 2:
+        server_ip, server_port = sys.argv[2].split(":")
 
-    return server_ip,int(server_port)
+    return server_ip, int(server_port)
 
-#Function to create and bin the server socket 
-# exception to handle with socket errors 
-#def bind_socket(server_ip_and_port_tuple):
-def bind_socket(server_ip,server_port):
+# Function to create and bind the server socket
+def bind_socket(server_ip, server_port):
     try:
         # Create the socket and bind it to listen
         server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
- #       server_socket.bind((server_ip_and_port_tuple[0], server_ip_and_port_tuple[1]))
-        server_socket.bind(server_ip,server_port)
+        server_socket.bind((server_ip, server_port))
         return server_socket
     except socket.error as e:
         print(f"Socket error: {e}")
 
-
-#----------------------------------------------------------------------------------
-
-#this service1 functions is to the server create a socket and bind it in order to be able to listen the client's messages and then with threads 
-#sends it to service1_aux function to process the message and then answer to client
-
+# Service 1 auxiliary function
 def service1_aux(socket, remetente, mensagem):
     for i in range(5):
         time.sleep(2)
@@ -52,11 +39,10 @@ def service1_aux(socket, remetente, mensagem):
 
     socket.sendto("Eu também :)\n".encode(), remetente)
 
-def service1(wg):
+def service1(server_ip, server_port):
     try:
-        server_ip,server_port=rec_args(1)
         # Bind the socket
-        server_socket = bind_socket(rec_args(1))
+        server_socket = bind_socket(server_ip, server_port)
 
         # While loop to be able to listen for more than one message
         # Threads to be able to process more packets at the same time
@@ -64,19 +50,14 @@ def service1(wg):
             data, remetente = server_socket.recvfrom(1024)
             threading.Thread(target=service1_aux, args=(server_socket, remetente, data.decode())).start()
 
-    #handle with exceptions and close the socket in the end of the code block
+    # Handle with exceptions and close the socket at the end of the code block
     except Exception as e:
         print(f"An error occurred: {e}")
     finally:
         if server_socket:
-            server_socket.close() 
+            server_socket.close()
 
-
-
-
-#----------------------------------------------------------------------------------------------------------
-
-
+# Service 2 auxiliary function
 def service2_aux(socket, remetente, mensagem, db):
     with db.lock:
         del db.dados["coisas"]
@@ -86,48 +67,48 @@ def service2_aux(socket, remetente, mensagem, db):
         time.sleep(2)
         print("SUCESIUM\n")
 
-    socket.sendto("SUCESIUM\n", remetente)
+    socket.sendto("SUCESIUM\n".encode(), remetente)
 
-
-def service2(wg,db):
+def service2(server_ip, server_port, db):
     try:
         # Bind the socket
-        server_socket = bind_socket(rec_args(2))
+        server_socket = bind_socket(server_ip, server_port)
 
         # While loop to be able to listen for more than one message
         # Threads to be able to process more packets at the same time
         while True:
             data, remetente = server_socket.recvfrom(1024)
-            threading.Thread(target=service2_aux, args=(server_socket, remetente, data.decode())).start()
+            threading.Thread(target=service2_aux, args=(server_socket, remetente, data.decode(), db)).start()
 
-    #handle with exceptions and close the socket in the end of the code block
+    # Handle with exceptions and close the socket at the end of the code block
     except Exception as e:
         print(f"An error occurred: {e}")
     finally:
         if server_socket:
-            server_socket.close() 
+            server_socket.close()
 
-#------------------------------------------------------------------------------------------------------
-
-
-def servico3(wg, db):
+# Service 3 function
+def service3(db):
     while True:
         with db.lock:
             for key, value in db.dados.items():
                 time.sleep(2)
                 print(f"Chave: {key} || Valor inicial: {value} || Valor final: {db.dados[key]}\n")
 
-
-#--------------------------------------------------------------------------------------------------------
-
 if __name__ == "__main__":
     db = Database()
 
     threads = []
     wg = threading.Event()
-    threads.append(threading.Thread(target=service1, args=(wg,)))
-    threads.append(threading.Thread(target=service2, args=(wg, db)))
-    threads.append(threading.Thread(target=servico3, args=(wg, db)))
+
+    server_ip, server_port = rec_args(1)
+    threads.append(threading.Thread(target=service1, args=(server_ip, server_port)))
+    
+    server_ip, server_port = rec_args(2)
+    threads.append(threading.Thread(target=service2, args=(server_ip, server_port, db)))
+
+    # Start the service3 function in a separate thread
+    threads.append(threading.Thread(target=service3, args=(db,)))
 
     for thread in threads:
         thread.start()
