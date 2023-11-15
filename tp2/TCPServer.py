acd006469d0e78eba6420ac0afd69836ecdbedee
Daniel Du <73446163+72Rogan@users.noreply.github.com>
2023-11-15 19:33:52 +0000

    ja recebe os vizinhos

--- tp2/TCPServer.py
+++ tp2/TCPServer.py
@@ -1,13 +1,16 @@
 import socket
 import threading
-import time
 
 class TCPServer:
-    def __init__(self, server_ip, server_port):
+    def __init__(self, server_ip, server_port, bootstrap_ip, bootstrap_port, is_bootstrap):
         self.server_ip = server_ip
         self.server_port = server_port
+        self.bootstrap_ip = bootstrap_ip
+        self.bootstrap_port = int(bootstrap_port)
         self.wg = threading.Event()
         self.clients = set()
+        self.is_bootstrap = is_bootstrap
+        self.my_neighbours=set()
 
     def create_and_bind_socket(self):
         try:
@@ -15,35 +18,13 @@ class TCPServer:
             server_socket.bind((self.server_ip, self.server_port))
             return server_socket
         except socket.error as e:
-            print(f"TCP : Socket Error: {e}")
-    
-    '''
-    def tcp_aux(self, client_socket, client_address):
-        print(f"TCP : Connected to: {client_address}")
-        try:
-            while not self.wg.is_set():
-                data = client_socket.recv(1024)
-                if not data:
-                    break
-
-                for i in range(5):
-                    time.sleep(2)
-                    print(f"TCP : Received message from {client_address}: {data}")
-                    response = b"TCP : Server received your message: " + data
-                client_socket.send(response)
-        except Exception as e:
-            print(f"TCP : An error occurred: {e}")
-        finally:
-            print(f"TCP : Connection closed with {client_address}")
-            client_socket.close()
-            self.clients.remove(client_socket)
-    '''
+            print(f"TCP: Socket Error: {e}")
 
     def send_message(self, client_socket, message):
         try:
-            client_socket.send(message)
+            client_socket.send(message.encode())
         except Exception as e:
-            print(f"TCP : An error occurred while sending message: {e}")
+            print(f"TCP: An error occurred while sending message: {e}")
 
     def receive_messages(self, client_socket):
         try:
@@ -52,16 +33,43 @@ class TCPServer:
                 if not data:
                     break
 
-                print(f"TCP : Received message from client: {data}")
+                print(f"TCP: Received message from client: {data}")
                 # Handle the received message as needed
         except Exception as e:
-            print(f"TCP : An error occurred while receiving messages: {e}")
+            print(f"TCP: An error occurred while receiving messages: {e}")
 
+    def connect_to_bootstrap(self):
+        try:
+            client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            
+            client_socket.connect((self.bootstrap_ip, self.bootstrap_port))
+            
+            initial_message = "Hello Bootstrap"
+            self.send_message(client_socket, initial_message)
+
+            data = client_socket.recv(1024)
+            
+            decoded_data = data.decode()
+
+            self.my_neighbours=decoded_data
+            print(self.my_neighbours)
+
+            self.send_message(client_socket,"3")
+
+            
+        except Exception as e:
+            print(f"TCP: An error occurred while connecting to bootstrap: {e}")
+        finally:
+            if client_socket:
+                client_socket.close()
     def start(self):
         try:
             server_socket = self.create_and_bind_socket()
             server_socket.listen(5)
-            print(f"TCP : Listening on {self.server_ip}:{self.server_port}")
+            print(f"TCP: Listening on {self.server_ip}:{self.server_port}")
+
+            if not self.is_bootstrap:
+                self.connect_to_bootstrap()
 
             while not self.wg.is_set():
                 client_socket, client_address = server_socket.accept()
@@ -74,7 +82,7 @@ class TCPServer:
                 send_thread.start()
 
         except Exception as e:
-            print(f"TCP : An error occurred: {e}")
+            print(f"TCP: An error occurred: {e}")
         finally:
             for client_socket in self.clients:
                 client_socket.close()
--- tp2/bootstrap.py
+++ tp2/bootstrap.py
@@ -1,26 +1,24 @@
-# bootstrap.py
 import socket
 import threading
 import time
 import json  # for JSON serialization
-
+from Message import Message
 
 class Bootstrap:
-    def __init__(self,bootstrap_ip,bootstrap_port):
-        self.bootstrap_ip=bootstrap_ip
-        self.bootstrap_port=bootstrap_port
+    def __init__(self, bootstrap_ip, bootstrap_port):
+        self.bootstrap_ip = bootstrap_ip
+        self.bootstrap_port = bootstrap_port
         self.wg = threading.Event()
         self.threads = []
         self.dic_with_neighbours = {}
 
-    def read_neighbours_file(self,path):
-       with open(path, "r") as f:
+    def read_neighbours_file(self, path):
+        with open(path, "r") as f:
             data = json.load(f)
             for node in data["nodes"]:
                 ip = node["ip"]
                 neighbors = node["neighbors"]
                 self.dic_with_neighbours[ip] = neighbors
-
 
     def create_and_bind_socket(self):
         try:
@@ -30,26 +28,40 @@ class Bootstrap:
         except socket.error as e:
             print(f"Bootstrap : Socket Error: {e}")
 
-
-    '''
     def send_data(self, client_socket, client_address):
         while not self.wg.is_set():
-            # Implement your logic for sending data here
             time.sleep(2)
-            data_to_send = "Not a Node"
-            if(client_address[0] in self.dic_with_neighbours.keys()):
-                data_to_send = json.dumps(self.dic_with_neighbours[client_address[0]])
-            client_socket.send(data_to_send.encode())
+            #response = "NOT A NODE"
+            message = Message()
+
+            if client_address[0] in self.dic_with_neighbours:
+                message.addType(2)
+                message.addBody(json.dumps(self.dic_with_neighbours[client_address[0]]))
+
+            else:
+                message.addTypeAndBody(3,"YOU ARE NOT A NODE")
+
+            try:
+                client_socket.send(message)
+            except Exception as e:
+                print(f"BOOTSTRAP: An error occurred while sending data to {client_address}: {e}")
+                break
+
+
+    #def process_data(self, data, client_socket,client_address):
+    #    if data == 
 
     def receive_data(self, client_socket, client_address):
         while not self.wg.is_set():
-            # Implement your logic for receiving data here
             data = client_socket.recv(1024)
             if not data:
                 break
-            print(f"Bootstrap: Received message from {client_address}: {data.decode()}")
+            
+            print(f"BOOTSTRAP: Received message from {client_address}: {data.decode()}")
 
-
+            if data.decode() == "3":
+                self.wg.set() 
+     #       self.process_data(data, client_socket,client_address)
 
     def bootstrap(self, client_socket, client_address):
         print(f"BOOTSTRAP: Connected to: {client_address}")
@@ -69,57 +81,25 @@ class Bootstrap:
             print(f"BOOTSTRAP: Connection closed with {client_address}")
             client_socket.close()
 
-    '''
-    def bootstrap(self, client_socket, client_address):
-        print(f"BOOTSTRAP: Connected to: {client_address}")
-        try:
-            while not self.wg.is_set():
-                data = client_socket.recv(1024)
-                if not data:
-                    break
-                print(type(self.dic_with_neighbours.keys()))
-                print(type(client_address[0])) 
-                for i in range(5):
-                    time.sleep(2)
-                    print(f"BOOTSTRAP: Received message from {client_address}: {data}")
-                    response = "NOT A NODE"
-
-                    print(client_address[0])
-                    print(self.dic_with_neighbours.keys())
-                    print(self.dic_with_neighbours)
-                    if(client_address[0] in self.dic_with_neighbours.keys()):
-                        print("oalalalala")
-                        response = json.dumps(self.dic_with_neighbours[client_address[0]])
-
-                client_socket.send(response.encode())
-        except Exception as e:
-            print(f"BOOTSTRAP : An error occurred in the bootstrap function: {e}")
-        finally:
-            print(f"BOOTSTRAP : Connection closed with {client_address}")
-            client_socket.close()
-            #self.clients.remove(client_socket)
-    
-
     def start(self):
         try:
             server_socket = self.create_and_bind_socket()
-            server_socket.listen(5)
+            server_socket.listen(10)
             print(f"BOOTSTRAP: Listening on {self.bootstrap_ip}:{self.bootstrap_port}")
 
             self.read_neighbours_file("bootstrapteste.json")
+            print(self.dic_with_neighbours)
 
             while not self.wg.is_set():
                 client_socket, client_address = server_socket.accept()
-                '''
-                self.threads.append(threading.Thread(target=self.bootstrap, args=(client_socket, client_address)))
-                for thread in self.threads:
-                    thread.start()
-                '''
                 thread = threading.Thread(target=self.bootstrap, args=(client_socket, client_address))
                 thread.start()
-                
+
         except Exception as e:
             print(f"BOOTSTRAP: An error occurred in the start function: {e}")
         finally:
             if server_socket:
                 server_socket.close()
+
+
+
