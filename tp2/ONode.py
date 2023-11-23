7ffb8fe157937fca24da12b120a2920551a343a1
Daniel Du <73446163+72Rogan@users.noreply.github.com>
2023-11-23 09:47:31 +0000

    coisas

--- tp2/ONode.py
+++ tp2/ONode.py
@@ -4,25 +4,24 @@ import threading
 from ONodeTCP import ONodeTCP
 from ONodeUDP import ONodeUDP
 from bootstrap import Bootstrap
+from RP import RP
 
-def parserArgs(arg):
-    ip, porta = sys.argv[arg].split(":")
-    return ip, int(porta)
 
-class MainServer():
-    def __init__(self,tcp_server_ip, tcp_server_port, udp_server_ip, udp_server_port,bootstrap_ip,bootstrap_port,bootstrap_mode):
+class ONode():
+    def __init__(self, bootstrap_ip,bootstrap_mode):
         self.wg = threading.Event()
         self.threads = []
-        self.mode=bootstrap_mode
+        self.mode=bootstrap_mode    
         if self.mode:
-            self.bootstrap= Bootstrap(bootstrap_ip,bootstrap_port)
-        self.tcp_server = ONodeTCP(tcp_server_ip, tcp_server_port)
-        self.udp_server = ONodeUDP(udp_server_ip, udp_server_port)
+            self.bootstrap= Bootstrap()
+        self.ONode_tcp = ONodeTCP(bootstrap_ip,self.mode)
+        self.ONode_udp = ONodeUDP()
         
 
     def start(self):
-        self.threads.append(threading.Thread(target=self.tcp_server.start))
-        self.threads.append(threading.Thread(target=self.udp_server.start))
+        
+        self.threads.append(threading.Thread(target=self.ONode_tcp.start))
+        self.threads.append(threading.Thread(target=self.ONode_udp.start))
         if self.mode:
             self.threads.append(threading.Thread(target=self.bootstrap.start))
 
@@ -33,22 +32,28 @@ class MainServer():
         self.wg.set()
         for thread in self.threads:
             thread.join()
-        self.tcp_server.stop()
-        self.udp_server.stop()
+        self.ONode_tcp.stop()
+        self.ONode_udp.stop()
 
 if __name__ == "__main__":
     # in case bootstrap mode is 1, otherwise, it is equal to 0
     bootstrap_mode = 0
-    bootstrap_ip = bootstrap_port = None  # Initialize to None
+    rp_mode = 0
+    bootstrap_ip = "" # Initialize to None
 
-    if sys.argv[1] == "--b":
-        bootstrap_mode = 1
-        bootstrap_ip, bootstrap_port = parserArgs(2)
-        tcp_server_ip, tcp_server_port = parserArgs(3)
-        udp_server_ip, udp_server_port = parserArgs(4)
-    else:
-        tcp_server_ip, tcp_server_port = parserArgs(1)
-        udp_server_ip, udp_server_port = parserArgs(2)
+    if (len(sys.argv) <3):
+        if(sys.argv[1].lower() == "--b"):
+            bootstrap_mode = 1
+        elif(sys.argv[1].lower() == "--rp".)    
+            rp_mode = 1
+            #abordagem para saber os servidores era passar como um argumento
+        else:
+            bootstrap_ip = sys.argv[1]
+    if(len(sys.argv)>2 and len(sys.argv)<4):
+        if((sys.argv[1].lower() == "--rp" and sys.argv[2].lower() == "--b") or (sys.argv[2].lower() == "--rp" and sys.argv[1].lower() == "--b")):
+            bootstrap_mode=1
+            rp_mode = 1
 
-    main_server = MainServer(tcp_server_ip, tcp_server_port, udp_server_ip, udp_server_port, bootstrap_ip, bootstrap_port, bootstrap_mode)
-    main_server.start()
+
+    ONode = ONode(bootstrap_ip,bootstrap_mode)
+    ONode.start()
--- tp2/RP.py
+++ tp2/RP.py
@@ -5,61 +5,56 @@ import json  # for JSON serialization
 import queue
 from Message import Message
 
-class Bootstrap:
+class RP:
     def __init__(self, ):
         self.wg = threading.Event()
         self.threads = []
         self.dic_with_neighbours = {}
         self.lock = threading.Lock()
         self.connected_clients = set()
-        self.bootstrap_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.rp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.receive_queue = queue.Queue()
         self.process_queue = queue.Queue()
         try:
-            self.bootstrap_socket.bind(("0.0.0.0", 3000))
+            self.rp_socket.bind(("0.0.0.0", 3000))
         except socket.error as e:
-            print(f"\nBootstrap : Socket Error: {e}")
+            print(f"\nRP : Socket Error: {e}")
 
 
-    def read_neighbours_file(self, path):
-        with open(path, "r") as f:
-            data = json.load(f)
-            for node in data["nodes"]:
-                ip = node["ip"]
-                neighbors = node["neighbors"]
-                self.dic_with_neighbours[tuple(ip)] = neighbors
 
     #Função para receber mensagens de um dado socket.
     #Ao receber, coloca na queue de mensagens recebidas para serem futuramente processadas
     def receive_messages(self):
         try:
             while True:
-                client_socket, client_address = self.bootstrap_socket.accept()
-                print(f"\nBOOTSTRAP : CONNECTED WITH {client_address}")
+                client_socket, client_address = self.rp_socket.accept()
+                print(f"\nRP : CONNECTED WITH {client_address}")
                 self.connected_clients.add(client_socket)
                 data = client_socket.recv(1024)
                 client_address = client_socket.getpeername()
-                print(f"\nBOOTSTRAP : Received this message from {client_address}: {data}")
+                print(f"\nRP : Received this message from {client_address}: {data}")
                 if not data:
                     break
                 with self.lock:
                     self.receive_queue.put((data, client_socket))
-                    print(f"\nBOOTSTRAP : I HAVE {self.receive_queue.qsize()} ELEMENTS IN THE RECEIVE QUEUE")
+                    print(f"\nRP : I HAVE {self.receive_queue.qsize()} ELEMENTS IN THE RECEIVE QUEUE")
                     
         except Exception as e:
-            print(f"\nBOOTSTRAP : An error occurred while receiving messages: {e}")
+            print(f"\nRP : An error occurred while receiving messages: {e}")
 
     #Função para processar as mensagens que estão na queue de mensagens recebidas
     #Apos o processamento, ela coloca as mensagens já processadas numa queue de mensagens processadas 
     #para depois poderem ser enviadas 
+
+    #FIXMe MUDAR O PORCESSAMENTO DAS MENSAGENS 
     def process_messages(self):
         while True:
             with self.lock:
                 #print(self.receive_queue.qsize())
                 if not self.receive_queue.empty():
-                    print("\nBOOTSTRAP : GOING TO PROCESS A MESSAGE")
+                    print("\nRP : GOING TO PROCESS A MESSAGE")
                     data, client_socket = self.receive_queue.get()
-                    print(f"\nBOOTSTRAP : I HAVE {self.receive_queue.qsize()} ELEMENTS IN THE RECEIVE QUEUE")
+                    print(f"\nRP : I HAVE {self.receive_queue.qsize()} ELEMENTS IN THE RECEIVE QUEUE")
                     try:
                         # Deserialize the JSON data into a Python object
                         message_data = json.loads(data.decode())
@@ -79,26 +74,26 @@ class Bootstrap:
                         if message_data["id"] == "4":
                             self.connected_clients.remove(client_socket)
                             #client_socket.close()
-                            print(f"\nBOOTSTRAP : CLOSED A CONNECTION WITH {client_socket.getsockname()[0]}")
+                            print(f"\nRP : CLOSED A CONNECTION WITH {client_socket.getsockname()[0]}")
                             
 
                         # Handle other cases as needed...
                         self.process_queue.put((json.dumps(messagem.__dict__), client_socket,False))
-                        print(f"\nBOOTSTRAP : I HAVE {self.process_queue.qsize()} ELEMENTS IN THE PROCESS QUEUE")
+                        print(f"\nRP : I HAVE {self.process_queue.qsize()} ELEMENTS IN THE PROCESS QUEUE")
                     except json.JSONDecodeError as e:
-                        print(f"\nBOOTSTRAP : Error decoding JSON data: {e}")
+                        print(f"\nRP : Error decoding JSON data: {e}")
                     except Exception as e:
-                        print(f"\nBOOTSTRAP : Error in processing messages: {e}")
+                        print(f"\nRP : Error in processing messages: {e}")
 
     #Função para enviar as mensagens que estão na queue de mensagens já processadas
     def send_messages(self):
         while True:
             with self.lock:
                 if not self.process_queue.empty():
-                    print("\nBOOTSTRAP : GOING TO SEND A MESSAGE")
+                    print("\nRP : GOING TO SEND A MESSAGE")
                     #client_socket = None
                     data, client_socket, Socket_is_Created = self.process_queue.get()
-                    print(f"\nBOOTSTRAP : I HAVE {self.process_queue.qsize()} ELEMENTS IN THE PROCESS QUEUE")
+                    print(f"\nRP : I HAVE {self.process_queue.qsize()} ELEMENTS IN THE PROCESS QUEUE")
                     message_data = json.loads(data)
                     ip_destino = message_data["dest"][0]
                     port_destino = message_data["dest"][1]
@@ -110,20 +105,20 @@ class Bootstrap:
                         client_socket.send(data.encode())
                         print(f"\nTCP : Sent this message: {data} to: ({ip_destino},{port_destino})")
                     except json.JSONDecodeError as e:
-                        print(f"\nBOOTSTRAP : Error decoding JSON data: {e}")
+                        print(f"\nRP : Error decoding JSON data: {e}")
                     except Exception as e:
-                        print(f"\nBOOTSTRAP : Error sending message in the send_messages function: {e}")
+                        print(f"\nRP : Error sending message in the send_messages function: {e}")
                     finally:
                         if client_socket:
-                            print(f"\nBOOTSTRAP : CLOSED CONNECTOIN WITH {client_socket.getpeername()}")
+                            print(f"\nRP : CLOSED CONNECTOIN WITH {client_socket.getpeername()}")
                             client_socket.close()
                             
 
     def start(self):
         try:
-            #self.read_neighbours_file("bootstrapteste.json")
+            #self.read_neighbours_file("RPteste.json")
             self.read_neighbours_file("teste3.json")
-            self.bootstrap_socket.listen(5)
+            self.rp_socket.listen(5)
 
             while not self.wg.is_set():
 
@@ -141,10 +136,10 @@ class Bootstrap:
                 send_thread.join()
 
         except Exception as e:
-            print(f"\nBOOTSTRAP : An error occurred in start function: {e}")
+            print(f"\nRP : An error occurred in start function: {e}")
         finally:
             for client_socket in self.clients:
                 client_socket.close()
 
-            if self.bootstrap_socket:
-                self.bootstrap_socket.close()
+            if self.rp_socket:
+                self.rp_socket.close()
